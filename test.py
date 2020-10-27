c51b0d81979dd5dd08ce723c9dea0382ea02c345
Chuck Lu <chuck.lu@qq.com>
2020-10-28 01:10:58 +0800

    recognize the word pattern successfully

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-words = [u'安安静静', '绿油油', '吃饭', 'Python', '试试看', '考察考察', '横七竖八', '波光粼粼']
+words = ['安安静静', '绿油油', '吃饭', 'Python', '试试看', '考察考察', '横七竖八', '波光粼粼']
+dic = {1:'A',2:'B',3:'C'}
+templates = ['AABB','ABAB','ABCC','ABB','AAB','OTHER']
+tempDictionary = {}
 for word in words:
-    print(word)
+    template = ''
+    i = 0
     for ch in word:
-        print(ch)
-    print()
+        if ch not in tempDictionary:
+            i = i + 1
+            if(i > 3):
+                template = 'OTHER'
+                continue
+            tempDictionary[ch] = dic[i]
+        template = template + tempDictionary[ch]    
+    if template in templates:
+        print(word+'为'+template+'型')
+    else:
+        print(word+'为OTHER型')
