6637eca46519c3b2408551006631689de068c73b
Chuck Lu <chuck.lu@qq.com>
2020-10-28 00:38:11 +0800

    first test version for 1022 homework 5

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-def index(array, n):
-    length = len(array)
-    if(n > length - 1):
-        return -1
-    item = array[n]
-    return item ** n;
+words = [u'安安静静', '绿油油', '吃饭', 'Python', '试试看', '考察考察', '横七竖八', '波光粼粼']
+for word in words:
+    print(word)
+    for ch in word:
+        print(ch)
+    print()
