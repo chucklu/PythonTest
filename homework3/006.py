c3d03621fd21e460867a01b6b7d84fc20e1e4eba
Chuck Lu <chuck.lu@qq.com>
2020-11-01 21:34:21 +0800

    copy sample code

---
+++ homework3/006.py
@@ -0,0 +1,8 @@
+dict_id_021
+= {' id':'021 ', name':'John ', health_code ': 'green
+dict_id_022
+= {' id':'022 ', name Mary ', health_code red
+dict_id_023
+= {' id':'023 ', name':'Ann ', health_code ':'green
+dict_all
+= {'021 ' : dict_id_021 , '022 ' : dict_id_022 , '023 ' : dict_id_023
