66f1f34078bc26cae0d2489182603492e8861cba
Chuck Lu <chuck.lu@qq.com>
2020-11-01 21:55:08 +0800

    implement code with solution

--- homework3/006.py
+++ homework3/006.py
@@ -1,4 +1,17 @@
-dict_id_021 = {'id':'021', 'name':'John', 'health_code ': 'green'}
+dict_id_021 = {'id':'021', 'name':'John', 'health_code': 'green'}
 dict_id_022 = {'id':'022', 'name':'Mary', 'health_code': 'red'}
-dict_id_023 = {'id':'023', 'name':'Ann', 'health_code ':'green'}
+dict_id_023 = {'id':'023', 'name':'Ann', 'health_code':'green'}
 dict_all = {'021' : dict_id_021 , '022' : dict_id_022 , '023' : dict_id_023}
+
+inputList = ['022','023','024']
+for id in inputList:
+	if id in dict_all:
+	    tempDictionary = dict_all[id]
+	    healthCode = tempDictionary["health_code"]
+	    name = tempDictionary["name"]
+	    if(healthCode == "green"):
+	        print("允许%s进入" %name)
+	    else:
+	    	print("不允许%s进入" %name)
+	else:
+		print("user with id %s not exist" %id)
