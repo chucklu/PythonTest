67c08f5de45205f68323f5dfc151722cbef990fa
Chuck Lu <chuck.lu@qq.com>
2020-11-07 19:33:15 +0800

    print the file content by invoke print_quotes method

--- homework4/4-2-1.py
+++ homework4/4-2-1.py
@@ -2,6 +2,6 @@ name = "陆游"
 word = "此生谁料，心在天山，身老沧洲。"
 
 def print_quotes(name, word):
-    print(f"{name} once said, \"{word}\"")
+    print(f"{name} said, \"{word}\"")
 
 print_quotes(name,word)
--- homework4/4-2-3.py
+++ homework4/4-2-3.py
@@ -8,10 +8,16 @@ def read_person_words(file_path):
 	return result
 
 def parse(line):
+	line = line.strip()
 	words = line.split('\t')
 	result = (words[0], words[1])
 	return result
 
+def print_quotes(name, word):
+    print(f"{name} said, \"{word}\"")
+
 file_path = "./files/person_sayings.txt"
-read_person_words(file_path)
+results = read_person_words(file_path)
+for result in results:
+	print_quotes(result[0],result[1])
 
