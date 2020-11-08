d480284d0494dc7ba39aaf53c07c728881625f4c
Chuck Lu <chuck.lu@qq.com>
2020-11-08 14:40:26 +0800

    add 4-2-5-2.py for real bubble sort

---
+++ homework4/4-2-5-2.py
@@ -0,0 +1,26 @@
+#Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly steps through the list, 
+#compares adjacent elements and swaps them if they are in the wrong order.
+
+#assume you hava 5 elements in list [5,1,4,2,8]
+#descending order need to compare the last two elements first
+#loop1
+#[5,1,4,2,8]-->[5,1,4,8,2] compare element at index3 and element at index4, 2<8(not descending order), swap them
+#[5,1,4,8,2]-->[5,1,8,4,2] compare element at index2 and element at index3, 4<8, swap them
+#[5,1,8,4,2]-->[5,8,1,4,2] compare element at index1 and element at index2, 1<8, swap them
+#[5,8,1,4,2]-->[8,5,1,4,2] compare element at index0 and element at index1, 5<8, swap them
+#loop2, skip the element at index0
+#[8,5,1,4,2]-->[8,5,1,4,2] compare element at index3 and element at index4, 4>2(descending order), no need to swap
+#[8,5,1,4,2]-->[8,5,4,1,2] compare element at index2 and element at index3, 1<4(not descending order), swap them
+#[8,5,4,1,2]-->[8,5,4,1,2] compare element at index1 and element at index2, 5>4(descending order), no need to swap
+#loop3, skip the element at index0 and index1
+#[8,5,4,1,2]-->[8,5,4,2,1] compare element at index3 and element at index4, 1<2(not descending order), swap them
+#[8,5,4,2,1]-->[8,5,4,2,1] compare element at index2 and element at index3, 4>2(descending order), no need to sap them
+#loop4, skip the element at index0 and index1 and index2
+#[8,5,4,2,1]-->[8,5,4,2,1] compare element at index3 and element at index4, 2>1(descending order), no need to sap them
+	
+def sort_nums(data_list):
+
+#data_list = [9, 23, 10, 217, 11, 23, 55, 79, 196, 224, 56, 135]
+data_list = [5,1,4,2,8]
+result = sort_nums(data_list)
+print(result)
