349f3a65cdc7361bf3abb9d61da93d9a47b51487
Chuck Lu <chuck.lu@qq.com>
2020-11-24 21:23:25 +0800

    consider two punctuation come together

--- homework5/hwk5_split_sentences.py
+++ homework5/hwk5_split_sentences.py
@@ -5,16 +5,21 @@ def split_cn_sents(txt):
     temp = ""
     for i in range(0,ch_count,1):
     	ch = txt[i]
-    	temp = f"{temp}{ch}"
-    	if(ch in puncts):
-    		sents.append(temp)
-    		temp = ""
+    	if(ch in puncts and temp==""):
+    		sents_count = len(sents)
+    		index = sents_count-1
+    		sents[index] =  f"{sents[index]}{ch}"
+    	else:
+    		temp = f"{temp}{ch}"
+    		if(ch in puncts):
+    			sents.append(temp)
+    			temp = ""
     return sents
 
 def test():
     txt = '治国凭圭臬，安邦靠准绳。厉行法治，重在法之必行！！'
     sents = split_cn_sents(txt)
     print(sents)
-    #sents = ['治国凭圭臬，安邦靠准绳。', '厉行法治，重在法之必行。']
+    #sents = ['治国凭圭臬，安邦靠准绳。', '厉行法治，重在法之必行！！']
 
 test()
