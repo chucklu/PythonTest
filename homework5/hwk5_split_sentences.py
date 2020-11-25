de768519d342866e90930256c1e63cd5a686be81
Chuck Lu <chuck.lu@qq.com>
2020-11-25 23:18:43 +0800

    refactor 5-3

--- homework5/hwk5_split_sentences.py
+++ homework5/hwk5_split_sentences.py
@@ -1,24 +1,43 @@
 def split_cn_sents(txt):
     puncts = set('；。！…？')
     sents = []
+    end_index_list = []
     ch_count = len(txt)
-    start_index = 0
-    end_index = 0
     for i in range(0,ch_count,1):
-    	ch = txt[i]
-    	if(ch in puncts and i==start_index):
-    		sents_count = len(sents)
-    		index = sents_count-1
-    		sents[index] =  f"{sents[index]}{ch}"
-    		start_index += 1
-    	else:
-    		if(ch in puncts):
-    			#print(f"i = {i}, start_index = {start_index}")
-    			end_index = i+1
-    			sent = txt[start_index:end_index]
-    			sents.append(sent)
-    			start_index = i+1
+        ch = txt[i]
+        if(ch in puncts):
+            end_index_list.append(i)
+            #print(i)
+    #print(end_index_list)
+    real_end_index_list = remove_duplicate_end_index(end_index_list)
+    #print(real_end_index_list)
+    end_index_count = len(real_end_index_list)
+
+    start_index = 0
+    for i in range(0,end_index_count):
+        end_index = real_end_index_list[i]
+        sent = txt[start_index:end_index+1]
+        start_index = end_index+1
+        #print(sent)
+        sents.append(sent)
     return sents
+
+def remove_duplicate_end_index(end_index_list):
+    count =  len(end_index_list)
+    has_popped = False
+    #print(f"count={count}")
+    for i in range(0,count-1,1):
+        #print(f"i={i}")
+        if(end_index_list[i]+1==end_index_list[i+1]):
+            end_index_list.pop(i)
+            has_popped = True
+            break
+    if(has_popped==True):
+        return remove_duplicate_end_index(end_index_list)
+    else:
+        #print(end_index_list)
+        return end_index_list
+
 
 def test():
     txt = '治国凭圭臬，安邦靠准绳。厉行法治，重在法之必行！！'
