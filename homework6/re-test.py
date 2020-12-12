9436687c5cc1d8c99735f6b99538e10ade3d2684
Chuck Lu <chuck.lu@qq.com>
2020-12-12 23:45:07 +0800

    access the group of match by group name

--- homework6/re-test.py
+++ homework6/re-test.py
@@ -1,10 +1,11 @@
 import re
-rgx_pat_phone_no = r'^((13\d)(\d{8}))'
-txt = '13312345679'
-mat = re.fullmatch(rgx_pat_phone_no, txt)
-print(mat)
-if mat:
-	for i in range(0,4):
-		group = mat.group(i)
-		print(f'group({i}) = {group}')
-		print(f'group({i}) start from {mat.start(i)}, end at {mat.end(i)}')
+year_pattern = r'(?P<year>\d{4})年'
+month_pattern = r'(?P<month>\d{1,2})月'
+day_pattern = r'(?P<day>\d{1,2})日'
+patterns_dic = {'year':year_pattern, 'month':month_pattern, 'day':day_pattern}
+txt = '他出生在2020年3月。'
+for key in patterns_dic:
+	mat = re.search(patterns_dic[key], txt)
+	print(mat)
+	if mat:
+		print(f'{mat.group(key)}')
