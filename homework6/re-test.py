3b2c3eca84b77f9fa312c42e52f8b36b341bdf62
Chuck Lu <chuck.lu@qq.com>
2020-12-12 23:59:37 +0800

    reuse group by group number

--- homework6/re-test.py
+++ homework6/re-test.py
@@ -1,6 +1,6 @@
 import re
 txt = 'John is the the instructor.'
-rgx_pat = r'\b(?P<word>\w+)\s+(?P=word)'
+rgx_pat = r'\b(?P<word>\w+)\s+\1'
 mat = re.search(rgx_pat, txt)
 if mat:
 	print(mat.group('word'))
