97f52e7de840b2b8b5a3be82d200ff410f6ef145
Chuck Lu <chuck.lu@qq.com>
2020-12-13 00:59:49 +0800

    look ahead match

--- homework6/re-test.py
+++ homework6/re-test.py
@@ -3,3 +3,11 @@ txt = '<hello>,hello!'
 rgx_pat = r'(<)?hello(?(1)>|!)'
 for mat in re.finditer(rgx_pat, txt):
 	print(mat.group())
+
+rgx_pat2 = 'hello(?=!)'
+for mat in re.finditer(rgx_pat2, txt):
+	print(mat.span(), mat.group())
+
+rgx_pat3 = 'hello(?!!)'
+for mat in re.finditer(rgx_pat3, txt):
+	print(mat.span(), mat.group())
