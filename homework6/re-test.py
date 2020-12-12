643e179aaa63343b19339c9ccb89f97ae204a689
Chuck Lu <chuck.lu@qq.com>
2020-12-13 00:13:28 +0800

    conditional match (page 39/54)

--- homework6/re-test.py
+++ homework6/re-test.py
@@ -1,6 +1,5 @@
 import re
-txt = 'John is the the instructor.'
-rgx_pat = r'\b(?P<word>\w+)\s+\1'
-mat = re.search(rgx_pat, txt)
-if mat:
-	print(mat.group('word'))
+txt = '<hello>,hello!'
+rgx_pat = r'(<)?hello(?(1)>|!)'
+for mat in re.finditer(rgx_pat, txt):
+	print(mat.group())
