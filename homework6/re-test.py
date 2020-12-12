03ce5465b4398bacfcf8c931cc51961458d2f84e
Chuck Lu <chuck.lu@qq.com>
2020-12-12 23:20:52 +0800

    test the group of regex on page 35/54

--- homework6/re-test.py
+++ homework6/re-test.py
@@ -1,8 +1,8 @@
 import re
-rgx_pat = 'a(b|c)*d'
-txt = 'ad,abd,acd,abbd,abcd,accd,acbccd,aed'
-mat = re.findall(rgx_pat, txt)
+rgx_pat_phone_no = r'^((13\d)(\d{8}))'
+txt = '13312345679'
+mat = re.fullmatch(rgx_pat_phone_no, txt)
 print(mat)
-
-for mat in re.finditer(rgx_pat, txt):
-    print(mat)
+if mat:
+	for i in range(0,4):
+		print(f'group({i}) = {mat.group(i)}')
