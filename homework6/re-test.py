b090b4b62e92df1f45aa62fb6d5b3c6a2b0b1957
Chuck Lu <chuck.lu@qq.com>
2020-12-12 23:57:15 +0800

    find the sequential same word by group reference (page 38/54)

--- homework6/re-test.py
+++ homework6/re-test.py
@@ -1,11 +1,6 @@
 import re
-year_pattern = r'(?P<year>\d{4})年'
-month_pattern = r'(?P<month>\d{1,2})月'
-day_pattern = r'(?P<day>\d{1,2})日'
-patterns_dic = {'year':year_pattern, 'month':month_pattern, 'day':day_pattern}
-txt = '他出生在2020年3月。'
-for key in patterns_dic:
-	mat = re.search(patterns_dic[key], txt)
-	print(mat)
-	if mat:
-		print(f'{mat.group(key)}')
+txt = 'John is the the instructor.'
+rgx_pat = r'\b(?P<word>\w+)\s+(?P=word)'
+mat = re.search(rgx_pat, txt)
+if mat:
+	print(mat.group('word'))
