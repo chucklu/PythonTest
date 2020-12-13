0398b0a7df65112ac038338d32ef53c34680afc8
Chuck Lu <chuck.lu@qq.com>
2020-12-13 15:36:56 +0800

    filter "给V"

--- homework6/re-test.py
+++ homework6/re-test.py
@@ -1,6 +1,6 @@
 import re
-txt = '这句话既有English，又有Chinese。'
-rgx_pat= r'[\u4e00-\u9fa5]' #简体汉字
+txt = '把/BA 我/PN 的/DEG 这些/DT 小/JJ 警察/NN 给/VV 吓走/VV 了/AS'
+rgx_pat= r'\s+给/[A-Z]+\s+[\u4e00-\u9fa5]+/V[A-Z]*\s+'
 rgx = re.compile(rgx_pat)
 for mat in rgx.finditer(txt):
 	print(mat.group(0))
