3fdd1e818dde2d744e1a874a7a763b245ff2970a
Chuck Lu <chuck.lu@qq.com>
2020-12-13 15:38:25 +0800

    use group name for verb

--- homework6/re-test.py
+++ homework6/re-test.py
@@ -1,6 +1,6 @@
 import re
 txt = '把/BA 我/PN 的/DEG 这些/DT 小/JJ 警察/NN 给/VV 吓走/VV 了/AS'
-rgx_pat= r'\s+给/[A-Z]+\s+[\u4e00-\u9fa5]+/V[A-Z]*\s+'
+rgx_pat= r'\s+给/[A-Z]+\s+(?P<vb>[\u4e00-\u9fa5]+/V[A-Z]*)\s+'
 rgx = re.compile(rgx_pat)
 for mat in rgx.finditer(txt):
 	print(mat.group(0))
