e7ce81891f89deea9116bbe6cecfab8d5aae729f
Chuck Lu <chuck.lu@qq.com>
2020-12-13 14:58:33 +0800

    search with position

--- homework6/re-test.py
+++ homework6/re-test.py
@@ -2,5 +2,7 @@ import re
 rgx_pat = 'a(b|c)*d'
 rgx = re.compile(rgx_pat, flags=re.IGNORECASE)
 txt = 'ad,abd,acd,abbd,abcd,ACCD,acbccd,aed'
-for mat in rgx.finditer(txt):
-	print(mat.group())
+mat = rgx.search(txt, pos=10)
+print(mat)
+mat_1 = rgx.search(txt, endpos=2)
+print(mat_1)
