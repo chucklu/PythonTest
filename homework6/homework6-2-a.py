abf3a9eb2acad6b06e79a8901d2d7861897f9924
Chuck Lu <chuck.lu@qq.com>
2020-12-13 16:50:15 +0800

    homework6-2-a.py

---
+++ homework6/homework6-2-a.py
@@ -0,0 +1,26 @@
+import re
+#可能/AD 会/VV 把/BA 我们/PN 给/VV 卖了/NN
+def count_gei_V(verb_dict, mat):
+	if(mat==None):
+		return
+	print(mat)
+	verb = mat.group('verb')
+	if verb not in verb_dict:
+		verb_dict[verb] = 0
+	verb_dict[verb] += 1
+
+def extract_gei_V(infile):
+	rgx_pat = r'\s+(?P<verb>[\u4e00-\u9fa5]+[起下上住到]+/v[A-Z]*)\s+'
+	rgx = re.compile(rgx_pat)
+	fin =  open(infile, 'r', encoding='utf-8')
+	verb_dict = {}
+	for line in fin:
+		for mat in rgx.finditer(line):
+			count_gei_V(verb_dict, mat)
+	fin.close()
+	return verb_dict
+
+infile = './files/sample_corpus_with_pos.txt'
+verb_dict = extract_gei_V(infile)
+for item in verb_dict:
+	print(item, verb_dict[item])
