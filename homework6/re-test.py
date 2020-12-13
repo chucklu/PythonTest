44b83b5a2105cea31aeb7bd83880021c41a56085
Chuck Lu <chuck.lu@qq.com>
2020-12-13 15:03:32 +0800

    match test

--- homework6/re-test.py
+++ homework6/re-test.py
@@ -2,7 +2,7 @@ import re
 rgx_pat = 'a(b|c)*d'
 rgx = re.compile(rgx_pat, flags=re.IGNORECASE)
 txt = 'ad,abd,acd,abbd,abcd,ACCD,acbccd,aed'
-mat = rgx.search(txt, pos=10)
+mat = rgx.match(txt, pos=3)
 print(mat)
-mat_1 = rgx.search(txt, endpos=2)
+mat_1 = rgx.match(txt, endpos=1)
 print(mat_1)
