c0635f181f6467bd7e2d8afd4b5f617dea778833
Chuck Lu <chuck.lu@qq.com>
2020-12-13 18:42:10 +0800

    save the result to files

--- homework6/homework6-3.py
+++ homework6/homework6-3.py
@@ -2,17 +2,12 @@ import requests
 import re
 
 def get_webpage(url):
+    #print('satrt get_webpage')
     x = requests.get(url, verify=False)
     # Show the status code: 200 means "OK". 404 means "page not found."
     print(url)
     print('Response:', x.status_code)
     return x.text
-
-def save_news_titles(titles, outfile, encoding='utf-8'):
-    my_fie = open(outfile,'w',-1,encoding)
-    for title in titles:
-        my_fie.write(f"{title}\n")
-    my_fie.close()
 
 def extract_title(content):
     #print(content)
@@ -23,21 +18,42 @@ def extract_title(content):
     title = mat.group('title')
     return title
 
-# def extract_article(content):
-#     article = ''
-#     return article
+def extract_post_time(content):
+    time = ''
+    return time
 
-def test_webpage_request():
-    url = 'https://www.reuters.com/article/us-health-coronavirus-usa-records/u-s-covid-19-cases-cross-11-million-as-pandemic-intensifies-idUSKBN27V0RX'
+def extract_post_source(content):
+    source = ''
+    return source
+
+def extract_text(content):
+    text = ''
+    return text
+
+def export(url, outfile):
+    #print(f'start export {url}, {outfile}')
     content = get_webpage(url)
-    print(content)
+    title = extract_title(content)
+    time = extract_post_time(content)
+    source = extract_post_source(content)
+    text = extract_text(content)
+    fout = open(outfile,'w',encoding='utf-8')
+    fout.write(title)
+    fout.write(time)
+    fout.write(source)
+    fout.write(text)
+    fout.close()
+    #print('end export')
+
 
 def test():
     urls = ['https://news.163.com/20/1116/09/FRHVKCJH000189FH.html?clickfrom=w_yw',
             'https://dy.163.com/article/FRLH5NV30532B1TX.html']
+    i = 0
+    for url in urls:
+        i += 1
+        outfile = f'./files/export_web_{i}.txt'
+        export(url, outfile)
     titles = [extract_title(get_webpage(url)) for url in urls]
-    outfile = './files/webpage_titles.txt'
-    save_news_titles(titles, outfile)
 
-#test_webpage_request()
 test()
