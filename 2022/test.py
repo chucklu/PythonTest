27010a6613cd27abf8c6bc55769c5d68d7eeb9e6
Chuck LU <chucklu2015@outlook.com>
2022-09-03 12:21:19 +0800

    add test for Fibonacci number

---
+++ 2022/test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# name=input("请输入姓名：")
+# print(name)
+
+a, b = 0, 1
+while (b < 1000):
+    print(a, b)
+    a = b
+    b = a+b
