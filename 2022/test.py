2b4b8aa45a3d73040c8ccf1600bb22b304037fc2
Chuck LU <chucklu2015@outlook.com>
2022-09-03 12:33:51 +0800

    test format string

--- 2022/test.py
+++ 2022/test.py
@@ -5,11 +5,13 @@ Spyder Editor
 This is a temporary script file.
 """
 
-# name=input("请输入姓名：")
-# print(name)
+name=input("请输入姓名：")
+print("你好，{0}".format(name))
+print()
 
 a, b = 0, 1
 while (b < 1000):
     print(a, b)
-    a = b
+    c = b
     b = a+b
+    a = c
