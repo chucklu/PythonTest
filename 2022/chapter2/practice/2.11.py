37c755269a6d680adb758ffbcfa7eec8b1285e48
Chuck LU <chucklu2015@outlook.com>
2022-09-17 20:48:29 +0800

    彩色靶盘

--- 2022/chapter2/practice/2.11.py
+++ 2022/chapter2/practice/2.11.py
@@ -1,11 +1,28 @@
 from turtle import *
+import numpy as np
+
+# Creating the screen object
+screen = Screen()
+
+# Setting the screen color-mode
+screen.colormode(255)
+
+def setRandomFillColor():
+    color = tuple(np.random.randint(0, 256, 3))
+    print(color)
+    fillcolor(color)
+    return color
 
 def drawCircle(r):
     up()
     home()
     setpos(pos()+(0, -r))
     down()
+
+    setRandomFillColor()
+    begin_fill()
     circle(r)
+    end_fill()
 
 radius = 200
 count = 9
