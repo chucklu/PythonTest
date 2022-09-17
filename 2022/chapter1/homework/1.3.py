02c4580a6138f5e3064d73fd435f3220f09381c6
Chuck LU <chucklu2015@outlook.com>
2022-09-17 11:51:52 +0800

    九九乘法表只需要打印一半

--- 2022/chapter1/homework/1.3.py
+++ 2022/chapter1/homework/1.3.py
@@ -1,5 +1,6 @@
 # 九九乘法表
 for i in range(1, 10):
     for j in range(1, 10):
-        print("{}*{}={:2} ".format(j, i, i*j), end=' ')
+        if (j <= i):
+            print("{}*{}={:2} ".format(j, i, i*j), end=' ')
     print()
