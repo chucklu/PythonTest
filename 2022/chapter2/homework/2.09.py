535e847500941c8c4d2d82ac44812bd6578972f6
Chuck LU <chucklu2015@outlook.com>
2022-09-17 19:51:26 +0800

    画上半圆

--- 2022/chapter2/homework/2.09.py
+++ 2022/chapter2/homework/2.09.py
@@ -4,14 +4,16 @@ step = 40
 length = 40
 count = 0
 pensize(25)
-while True:
-    if (count >= 2):
-        length += step
-        count = 0
-    left(90)
-    circle(length,180)
-    count = count+1
-    sum += 1
-    if (sum >= 39):
-        break
+left(90)
+circle(length,180)
+# while True:
+#     if (count >= 2):
+#         length += step
+#         count = 0
+#     left(180)
+#     circle(length,180)
+#     count = count+1
+#     sum += 1
+#     if (sum >= 39):
+#         break
 done()
