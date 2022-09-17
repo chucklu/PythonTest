09c585eddf2c00c1161c5868b2688a905090e1d6
Chuck LU <chucklu2015@outlook.com>
2022-09-17 19:18:51 +0800

    set color mode and generate random color

--- 2022/chapter2/homework/2.03.py
+++ 2022/chapter2/homework/2.03.py
@@ -1,5 +1,13 @@
 from turtle import *
+import numpy as np
 setup(650, 350, 200, 200)
+
+# Creating the screen object
+screen = Screen()
+
+# Setting the screen color-mode
+screen.colormode(255)
+
 penup()
 fd(-250)
 pendown()
@@ -7,8 +15,16 @@ pensize(25)
 pencolor("purple")
 seth(-40)
 for i in range(4):
+    color = tuple(np.random.random(size=3) * 256)
+    pencolor(color)
     circle(40, 80)
+
+    color = tuple(np.random.random(size=3) * 256)
+    pencolor(color)
     circle(-40, 80)
+
+color = tuple(np.random.random(size=3) * 256)
+pencolor(color)
 circle(40, 80/2)
 fd(40)
 circle(16, 180)
