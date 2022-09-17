af7dc0df5cf5bae5137c537e8ec9d1dde42e6c38
Chuck LU <chucklu2015@outlook.com>
2022-09-17 13:45:40 +0800

    角度设置为5度

--- 2022/chapter1/homework/1.8.py
+++ 2022/chapter1/homework/1.8.py
@@ -1,10 +1,14 @@
 # 太阳花的绘制
 from turtle import *
 color('red', 'yellow')
+sum = 0
 begin_fill()
 while True:
     forward(200)
-    left(170)
+    sum = sum+1
+    #left(170)  # 10,所以会绘制36次
+    left(175) # 5度,绘制360/5=72次
     if (abs(pos())) < 1:
         break
 end_fill()
+print("sum = {}".format(sum))
