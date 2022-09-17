e8a5e917c7561edc78234c1daeecffe71aff68ce
Chuck LU <chucklu2015@outlook.com>
2022-09-17 12:54:54 +0800

    正五边形的绘制

---
+++ 2022/chapter1/homework/1.7 - test.py
@@ -0,0 +1,12 @@
+# 正五边形的绘制
+from turtle import *
+fillcolor("red")
+print("position:{}, heading:{}".format(pos(),heading()))
+begin_fill()
+while True:
+    forward(200)
+    right(288)
+    print("position:{}, heading:{}".format(pos(),heading()))
+    if (abs(pos())) < 1:
+        break
+end_fill()
