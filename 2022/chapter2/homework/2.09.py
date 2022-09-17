fe247f455d756d8e3b46b578988e9673d0c0342a
Chuck LU <chucklu2015@outlook.com>
2022-09-17 20:22:26 +0800

    变色蚊香

--- 2022/chapter2/homework/2.09.py
+++ 2022/chapter2/homework/2.09.py
@@ -8,25 +8,41 @@ step = 20
 length = 10
 
 
-def randomColor():
+def setRandomPenColor():
     color = tuple(np.random.randint(0, 256, 3))
     print(color)
+    pencolor(color)
     return color
+
+
+def drawSnake():
+    global length
+    global step
+    length += step
+    setRandomPenColor()
+    circle(length, 60)
+
+    setRandomPenColor()
+    circle(length, 60)
+
+    setRandomPenColor()
+    circle(length, 60)
+    #print(heading())
+
+
+# Creating the screen object
+screen = Screen()
+
+# Setting the screen color-mode
+screen.colormode(255)
 
 
 pensize(15)
 left(90)
 
 while True:
-    length += step
-    circle(length, 180)
-    print(heading())
-
-    length += step
-    circle(length, 180)
-    print(heading())
-
+    drawSnake()
     sum += 1
-    if (sum >= 3):
+    if (sum >= 7):
         break
 done()
