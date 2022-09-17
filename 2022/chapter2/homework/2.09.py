9c682f5365c6e673d36a76360dffbebff81afe94
Chuck LU <chucklu2015@outlook.com>
2022-09-17 19:57:53 +0800

    爱心?

--- 2022/chapter2/homework/2.09.py
+++ 2022/chapter2/homework/2.09.py
@@ -1,15 +1,29 @@
+import imp
 from turtle import *
+import numpy as np
+
 sum = 0
 step = 40
 length = 40
 count = 0
+
+def randomColor():
+    color = tuple(np.random.randint(0,256,3))
+    print(color)
+    return color
+
 pensize(25)
 left(90)
 
-circle(length, 180)
-print(heading())
-circle(length*2, 180)
-print(heading())
+sum=0
+while True:
+    circle(length, 180)
+    print(heading())
+    circle(length*2, 180)
+    print(heading())
+    sum+=1
+    if(sum>=10):
+        break
 # while True:
 #     if (count >= 2):
 #         length += step
