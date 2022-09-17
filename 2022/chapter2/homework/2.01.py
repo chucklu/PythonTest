588a4a4246f4617091b753a1d39f7755fc041286
Chuck LU <chucklu2015@outlook.com>
2022-09-17 17:33:37 +0800

    eval the input

--- 2022/chapter2/homework/2.01.py
+++ 2022/chapter2/homework/2.01.py
@@ -1,4 +1,4 @@
-TempStr = input("请输入带有符号的温度值: ")
+TempStr = eval('input("请输入带有符号的温度值: ")')
 if TempStr[-1] in ['F','f']:
     C = (eval(TempStr[0:-1]) - 32)/1.8
     print("转换后的温度是{:.2f}C".format(C))
