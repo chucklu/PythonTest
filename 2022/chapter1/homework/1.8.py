93376ffdae79118e6cfecae4451b89936f2a4dac
Chuck LU <chucklu2015@outlook.com>
2022-09-17 14:13:59 +0800

    放大图形，并且打印坐标

--- 2022/chapter1/homework/1.8.py
+++ 2022/chapter1/homework/1.8.py
@@ -1,13 +1,15 @@
 # 太阳花的绘制
 from turtle import *
+setposition(-200,0)
 color('red', 'yellow')
 sum = 0
 begin_fill()
 while True:
-    forward(200)
+    forward(400)
     sum = sum+1
     #left(170)  # 10,所以会绘制36次
     left(175) # 5度,绘制360/5=72次
+    print("{},{},{}".format(sum,pos(),abs(pos())))
     if (abs(pos())) < 1:
         break
 end_fill()
