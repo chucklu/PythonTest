1936a7e982f467f9281762ce55c9449ed8d3ad65
Chuck LU <chucklu2015@outlook.com>
2022-09-17 19:41:01 +0800

    封装随机颜色函数，fd和circle之前都改变一次pencolor

--- 2022/chapter2/homework/2.03.py
+++ 2022/chapter2/homework/2.03.py
@@ -1,5 +1,12 @@
+from random import random
 from turtle import *
 import numpy as np
+
+def randomColor():
+    color = tuple(np.random.randint(0,256,3))
+    print(color)
+    return color
+
 setup(650, 350, 200, 200)
 
 # Creating the screen object
@@ -16,21 +23,28 @@ pensize(25)
 seth(-40)
 
 for i in range(4):
-    color = tuple(np.random.randint(0,256,3))
+    color = randomColor()
     pencolor(color)
     circle(40, 80)
 
-    color = tuple(np.random.randint(0,256,3))
+    color = randomColor()
     pencolor(color)
     circle(-40, 80)
 
-color = tuple(np.random.randint(0,256,3))
+color = randomColor()
 pencolor(color)
 circle(40, 80/2)
+
+color = randomColor()
+pencolor(color)
 fd(40)
 
-color = tuple(np.random.randint(0,256,3))
+color = randomColor()
 pencolor(color)
 circle(16, 180)
+
+color = randomColor()
+pencolor(color)
 fd(40 * 2/3)
+
 done()
