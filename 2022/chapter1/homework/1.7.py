c7502e394461a091822e66f5093ae15acddb8bea
Chuck LU <chucklu2015@outlook.com>
2022-09-17 12:36:45 +0800

    绘制五角星

---
+++ 2022/chapter1/homework/1.7.py
@@ -0,0 +1,12 @@
+# 五角星的绘制
+from turtle import *
+fillcolor("red")
+print("position:{}, heading:{}".format(pos(),heading()))
+begin_fill()
+while True:
+    forward(200)
+    right(144)
+    print("position:{}, heading:{}".format(pos(),heading()))
+    if (abs(pos())) < 1:
+        break
+end_fill()
