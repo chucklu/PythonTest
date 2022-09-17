c99ba5f18b9fe3a0d98f1fe4d7c617cea1f625f5
Chuck LU <chucklu2015@outlook.com>
2022-09-17 16:29:26 +0800

    六角形的绘制

---
+++ 2022/chapter2/homework/2.07.py
@@ -0,0 +1,26 @@
+from math import sqrt
+from turtle import *
+length = 300
+sum = 0
+setheading(270)
+while True:
+    forward(length)
+    sum += 1
+    if (sum >= 3):
+        break
+    left(120)
+
+up()
+goto(pos()+(-50*sqrt(3), -150))
+setheading(30)
+down()
+
+sum = 0
+while True:
+    forward(length)
+    sum += 1
+    if (sum >= 3):
+        break
+    right(120)
+
+done()
