625163e9b41693488c988ac2e9c767817b0ee929
Chuck LU <chucklu2015@outlook.com>
2022-09-17 19:53:20 +0800

    测试上半圆和下半圆

--- 2022/chapter2/homework/2.09.py
+++ 2022/chapter2/homework/2.09.py
@@ -5,7 +5,11 @@ length = 40
 count = 0
 pensize(25)
 left(90)
-circle(length,180)
+
+circle(length, 180)
+print(heading())
+circle(length*2, 180)
+print(heading())
 # while True:
 #     if (count >= 2):
 #         length += step
