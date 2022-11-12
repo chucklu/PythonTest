1236544a776025113de1a28746771efd68a7f6b6
Chuck LU <chucklu2015@outlook.com>
2022-11-12 10:39:49 +0800

    add comment and test string slice

--- 2022/chapter5/examples/5.3-2.py
+++ 2022/chapter5/examples/5.3-2.py
@@ -1,4 +1,10 @@
-s="hello"
-print(s)
-print(s[1:])
-print(s[:1])
+# https://realpython.com/python-strings/
+s = "foobar"
+print("{}的字符串长度为{}".format(s, len(s)))
+print(s[2:5])  # oba  从2开始，长度为5-2
+
+print(s[:4])  # foob 省略第一个参数，第一参数就是0
+print(s[0:4])
+
+print(s[2:])  # obar 省略第二个参数，第二个参数就是字符串的长度
+print(s[2:len(s)])
--- 2022/chapter5/examples/5.3.py
+++ 2022/chapter5/examples/5.3.py
@@ -1,7 +1,13 @@
 def reverse(s):
+    print("s = {}".format(s))
     if s == "":
         return s
     else:
-        return reverse(s[1:])+s[0]
-str=input("请输入一个字符串：")
+        temp = s[1:]  # 等价于s[1:len(s)]
+        print("temp = {}".format(temp))
+        temp2 = reverse(temp)+s[0]
+        print("temp2 = {}".format(temp2))
+        return temp2
+
+str = input("请输入一个字符串：")
 print(reverse(str))
