c8f0e8edb4c3dd15eaf8b4285c6a7670cfeaf4a8
Chuck LU <chucklu2015@outlook.com>
2022-11-12 16:41:58 +0800

    3种分词模式

--- 2022/chapter5/practice/6.15.py
+++ 2022/chapter5/practice/6.15.py
@@ -1,5 +1,12 @@
 # pip3 install jieba
 import jieba
-result = jieba.lcut("中华人民共和国是一个伟大的国家")
+str="中华人民共和国是一个伟大的国家"
+result = jieba.lcut(str)
 print(type(result))
+print(result)
+
+result = jieba.lcut(str,cut_all=True)
+print(result)
+
+result = jieba.lcut_for_search(str)
 print(result)
