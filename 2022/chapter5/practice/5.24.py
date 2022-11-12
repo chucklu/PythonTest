b9207b2461f93e3e3ab49b60551f187eaa6de2a6
Chuck LU <chucklu2015@outlook.com>
2022-11-12 14:39:13 +0800

    设置turtle颜色，来绘制科赫曲线

---
+++ 2022/chapter5/practice/5.24.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+
+def drawKoch(length, level):
+    tempLength = length/3.0
+    if (level == 0):
+        forward(length)
+        return
+    else:
+        for angle in [0, 60, -120, 60]:
+            left(angle)
+            drawKoch(tempLength, level-1)
+
+
+speed(0)
+pencolor("red")
+for angle in [0,120,120]:
+    right(angle)
+    drawKoch(300,3)
+hideturtle()
+done()
