9ba9c81162ee821e275e1c4327509aae913aaac9
Chuck LU <chucklu2015@outlook.com>
2022-11-19 10:14:46 +0800

    test with gb2312 encoding

--- 2022/chapter7/examples/7.1.py
+++ 2022/chapter7/examples/7.1.py
@@ -1,10 +1,16 @@
 
 import os
-#os.chdir('absolute-path-to-workingDir')
+# os.chdir('absolute-path-to-workingDir')
 path = os.path.abspath(os.getcwd())
 print("path is "+path)
 
-#textFile = open("7.1.txt", "r")
-textFile = open("7.1.txt", "r", encoding='UTF-8')
+fileName = "7.1-2.txt"
+
+textFile = open(fileName, "rt")  # chcp in cmd, Active code page: 936
+#textFile = open("7.1.txt", "rt", encoding='UTF-8')
 print(textFile.readline())
 textFile.close()
+
+binFile = open(fileName, "rb")
+print(binFile.readline())
+binFile.close()
