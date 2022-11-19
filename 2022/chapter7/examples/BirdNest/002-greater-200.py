1aab704cbf7267e5a532b91dce9cff4164c7f120
Chuck LU <chucklu2015@outlook.com>
2022-11-19 20:50:55 +0800

    set thresh hold as variable

--- 2022/chapter7/examples/BirdNest/002-greater-200.py
+++ 2022/chapter7/examples/BirdNest/002-greater-200.py
@@ -2,18 +2,19 @@ from PIL import Image
 im = Image.open('birdnest.jpg')
 print(im.mode)
 r, g, b = im.split()
-newr = r.point(lambda i: i > 200 and i or 0)  # 选择red通道值大于200的像素点
-newg = g.point(lambda i: i > 200 and i or 0)  # 选择green通道值大于200的像素点
-newb = b.point(lambda i: i > 200 and i or 0)  # 选择blue通道值大于200的像素点
+threshhold = 200
+newr = r.point(lambda i: i > threshhold and i or 0)  # 选择red通道值大于200的像素点
+newg = g.point(lambda i: i > threshhold and i or 0)  # 选择green通道值大于200的像素点
+newb = b.point(lambda i: i > threshhold and i or 0)  # 选择blue通道值大于200的像素点
 om = Image.merge(im.mode, (r, newg, newb))
-om.save('birdnestMerge-greater-100-keep-r.jpg')
+om.save('birdnestMerge-greater-{}-keep-r.jpg'.format(threshhold))
 
 om = Image.merge(im.mode, (newr, g, newb))
-om.save('birdnestMerge-greater-100-keep-g.jpg')
+om.save('birdnestMerge-greater-{}-keep-g.jpg'.format(threshhold))
 
 om = Image.merge(im.mode, (newr, newg, b))
-om.save('birdnestMerge-greater-100-keep-b.jpg')
+om.save('birdnestMerge-greater-{}-keep-b.jpg'.format(threshhold))
 
 om = Image.merge(im.mode, (newr, newg, newb))
-om.save('birdnestMerge-greater-100.jpg')
+om.save('birdnestMerge-greater-{}.jpg'.format(threshhold))
 
--- 2022/chapter7/examples/BirdNest/002-less-100.py
+++ 2022/chapter7/examples/BirdNest/002-less-100.py
@@ -2,17 +2,18 @@ from PIL import Image
 im = Image.open('birdnest.jpg')
 print(im.mode)
 r, g, b = im.split()
-newr = r.point(lambda i: i < 100 and i or 255)  # 选择red通道值低于100的像素点
-newg = g.point(lambda i: i < 100 and i or 255)  # 选择green通道值低于100的像素点
-newb = b.point(lambda i: i < 100 and i or 255)  # 选择blue通道值低于100的像素点
+threshhold = 100
+newr = r.point(lambda i: i < threshhold and i or 255)  # 选择red通道值低于100的像素点
+newg = g.point(lambda i: i < threshhold and i or 255)  # 选择green通道值低于100的像素点
+newb = b.point(lambda i: i < threshhold and i or 255)  # 选择blue通道值低于100的像素点
 om = Image.merge(im.mode, (r, newg, newb))
-om.save('birdnestMerge-less-100-keep-r.jpg')
+om.save('birdnestMerge-less-{}-keep-r.jpg'.format(threshhold))
 
 om = Image.merge(im.mode, (newr, g, newb))
-om.save('birdnestMerge-less-100-keep-g.jpg')
+om.save('birdnestMerge-less-{}-keep-g.jpg'.format(threshhold))
 
 om = Image.merge(im.mode, (newr, newg, b))
-om.save('birdnestMerge-less-100-keep-b.jpg')
+om.save('birdnestMerge-less-{}-keep-b.jpg'.format(threshhold))
 
 om = Image.merge(im.mode, (newr, newg, newb))
-om.save('birdnestMerge-less-100.jpg')
+om.save('birdnestMerge-less-{}.jpg'.format(threshhold))
