0dddf39766b60e76dc7d62f6d2507c34ba622f38
Chuck LU <chucklu2015@outlook.com>
2022-11-19 18:35:57 +0800

    character for Doraemon

--- 2022/chapter7/examples/12.1.py
+++ 2022/chapter7/examples/12.1.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import math
 # http://paulbourke.net/dataformats/asciiart/
+# 灰色值指黑白图像中的颜色深度，白色为255，黑色为0。
+# 白色的rgb是(255,255,255)，灰度值是255
 ascii_char = list(
     '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1\{\}[]?-_+~<>i!lI;:,"^`\'.')
 
@@ -18,19 +20,19 @@ def get_char(r, b, g, alpha=256):
 
 
 def main():
-    im = Image.open('astro.jpg')
-    WIDTH, HEIGHT = 270, 101
+    im = Image.open('doraemon.jpg')
+    WIDTH, HEIGHT = 103, 123
     im = im.resize((WIDTH, HEIGHT))
     txt = ""
     for i in range(HEIGHT):
         for j in range(WIDTH):
             txt += get_char(*im.getpixel((j, i)))
         txt += '\n'
-    fo = open("pic_char.txt", "w")
+    fo = open("doraemon.txt", "w")
     fo.write(txt)
     fo.close()
 
 
-ascii_char.reverse()
+# ascii_char.reverse() #黑色背景启用这个,
 main()
-#print(len(ascii_char))
+# print(len(ascii_char))
