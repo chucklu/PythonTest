28cbd39551b0ba5c12ba1af412b68c1eca5d5766
Chuck LU <chucklu2015@outlook.com>
2022-11-25 21:42:45 +0800

    adjust the comment

--- 2022/chapter7/examples/BirdNest/003.py
+++ 2022/chapter7/examples/BirdNest/003.py
@@ -2,6 +2,6 @@ from PIL import Image
 im = Image.open('birdnest.jpg')
 print(im.mode)
 r, g, b = im.split()
-newr = r.point(lambda i: 0)  # 将green通道颜色值变为原来的0.9倍
+newr = r.point(lambda i: 0)  # 移除红色通道
 om = Image.merge(im.mode, (newr, g, b))
 om.save('birdnestRemoveRed.jpg')
