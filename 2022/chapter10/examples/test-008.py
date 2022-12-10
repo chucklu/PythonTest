62bba4f4733fb94466d77def805563791ed7d9ac
Chuck LU <chucklu2015@outlook.com>
2022-12-10 17:57:53 +0800

    modify code

--- 2022/chapter10/examples/test-008.py
+++ 2022/chapter10/examples/test-008.py
@@ -6,34 +6,49 @@ from bs4 import BeautifulSoup
 with open("university.html", encoding='utf-8') as f:
     soup = BeautifulSoup(f, "html.parser")
 
-# find all <li> elements
-li_elements = soup.find_all('li')
+# Find all the <li> elements that contain the information we want to extract
+li_elements = soup.find_all("li", class_="item-list__ListItemStyled-sc-18yjqdy-1 boZDDO")
 
 # initialize the list that will hold the extracted information
 university_list = []
-# find all <li> elements with class "item-list__ListItemStyled-sc-18yjqdy-1 boZDDO"
-for li in soup.find_all("li", class_="item-list__ListItemStyled-sc-18yjqdy-1 boZDDO"):
-    # find the <a> element with class "Anchor-byh49a-0 Card__StyledAnchor-sc-1ra20i5-8 PlBer kijIJB card-name"
-    # that is under a <h3> element, and the <h3> element is under the current <li> element
-    a = li.find("h3").find("a", class_="Anchor-byh49a-0 Card__StyledAnchor-sc-1ra20i5-8 PlBer kijIJB card-name")
 
-    # find the <div> element with class "RankList__Rank-sc-2xewen-2 fxzjOx ranked has-badge"
-    # that is under the current <li> element
-    div = li.find("div", class_="RankList__Rank-sc-2xewen-2 fxzjOx ranked has-badge")
+# Create an empty list to store the extracted data
+university_list = []
 
-    # find all <dd> elements with class "QuickStatHug__Description-hb1bl8-1 eXguFl"
-    # that are under the current <li> element
-    dds = li.find_all("dd", class_="QuickStatHug__Description-hb1bl8-1 eXguFl")
+# Find all the <li> elements in the HTML
+li_elements = soup.find_all("li", class_="item-list__ListItemStyled-sc-18yjqdy-1 boZDDO")
 
-    # extract the rank, university name, tuition and fees, and undergraduate enrollment
-    # by accessing the text of the corresponding elements
-    rank = div.strong.text.replace("#", "")
-    university = a.text
-    tuition_and_fees = dds[0].text.replace("\n", "")
-    undergraduate_enrollment = dds[1].text.replace("\n", "")
+# Loop through each <li> element
+for li in li_elements:
+    # Extract the university name
+    university_name = li.find("a", class_="Anchor-byh49a-0 Card__StyledAnchor-sc-1ra20i5-8 PlBer kijIJB card-name").text
 
-    # append the data as a tuple to the university_list
-    university_list.append((rank, university, tuition_and_fees, undergraduate_enrollment))
+    # Extract the rank
+    rank = li.find("div", class_="RankList__Rank-sc-2xewen-2 fxzjOx ranked has-badge").text.replace("#", "")
 
-# print the university_list
-print(university_list)
+    # Extract the tuition and fees
+    tuition_fees = li.find_all("dd", class_="QuickStatHug__Description-hb1bl8-1 eXguFl")[0].text
+
+    # Extract the undergraduate enrollment
+    undergraduate_enrollment_dd = li.find_all("dd", class_="QuickStatHug__Description-hb1bl8-1 eXguFl")[1]
+    undergraduate_enrollment_p = undergraduate_enrollment_dd.find("p")
+    undergraduate_enrollment_p.extract()
+    undergraduate_enrollment = undergraduate_enrollment_dd.text
+
+    # Create a dictionary to store the extracted data for this university
+    university = {
+        "name": university_name,
+        "rank": rank,
+        "tuition_fees": tuition_fees,
+        "undergraduate_enrollment": undergraduate_enrollment,
+    }
+
+    # Append the dictionary to the university_list
+    university_list.append(university)
+
+# Print the university_list
+for university in university_list:
+    print(university["name"])
+    print(university["rank"])
+    print(university["tuition_fees"])
+    print(university["undergraduate_enrollment"])
