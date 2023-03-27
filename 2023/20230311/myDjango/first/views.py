09c597f74c9c6d6c0a11d3ab471be1a4215d3890
Chuck LU <chucklu2015@outlook.com>
2023-03-27 19:11:46 +0800

    use dict instead of set

--- 2023/20230311/myDjango/first/views.py
+++ 2023/20230311/myDjango/first/views.py
@@ -6,4 +6,4 @@ def index(request):
     return HttpResponse("这是我的第一个Django网页")
 
 def say_hello(request):
-    return render(request,"hello.html",{"name","Chuck"})
+    return render(request,"hello.html",{"name":"Chuck"})
