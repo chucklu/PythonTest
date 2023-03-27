3a02abb48bc67029187e68acc9c1df104902bbe9
Chuck LU <chucklu2015@outlook.com>
2023-03-27 19:59:03 +0800

    delete the chapter3 project, as we migrate it as an app under myDjango project

--- 2023/chapter3/testinclude/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from . import views as testincludeview
-urlpatterns =[
-    path("testinclude/",testincludeview.useincludetest)
-]
--- 2023/chapter3/testinclude/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def useincludetest(request):
-    return HttpResponse("这是应用testinclude下的useincludetest函数响应")
