7c81261777094894aa23a0491892ed2b64313ca0
Chuck LU <chucklu2015@outlook.com>
2023-04-13 15:30:55 +0800

    component is different from orphaned

--- 2023/GraphTest/graph-test-2023-0413-002.py
+++ 2023/GraphTest/graph-test-2023-0413-002.py
@@ -6,6 +6,8 @@ G = nx.DiGraph([(1,2), (2,3), (3,4), (4,5), (5,6), (6,3)])
 # add orphaned node
 G.add_node(7)
 
+G.add_edges_from([(8,9),(9,8)])
+
 # check for orphaned and looped nodes
 orphans = [n for n in G.nodes() if G.in_degree(n) == 0 and G.out_degree(n) == 0]
 loops = list(nx.simple_cycles(G))
