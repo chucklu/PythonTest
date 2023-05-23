db0846907601251090066ca3dfa5af4b00fd3b3e
Chuck LU <chucklu2015@outlook.com>
2023-05-23 17:23:56 +0800

    add url for test http://127.0.0.1:8000/test123

--- 2023/chapter5/chapter5/views.py
+++ 2023/chapter5/chapter5/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from datetime import date
-def showDate(request, urlData):
+def showData(request, urlData):
     d=date.today()
     s=f'urlData = {urlData}<br/>Today is {d}'
     return HttpResponse(s)
