911b51c2e2528887b7ca61d020818a33dd2683f6
Chuck LU <chucklu2015@outlook.com>
2023-05-23 19:19:10 +0800

    test http 404

--- 2023/chapter5/chapter5/urls.py
+++ 2023/chapter5/chapter5/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test<urlData>',views.showData)
+    path('testHttp404', views.testHttp404),
+    path('test<urlData>', views.showData),
 ]
--- 2023/chapter5/chapter5/views.py
+++ 2023/chapter5/chapter5/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from datetime import date
+from django.http import Http404
 def showData(request, urlData):
     d=date.today()
     s=f'urlData = {urlData}<br/>Today is {d}'
     return HttpResponse(s)
+
+def testHttp404(request):
+    raise Http404('This page does not exist')
