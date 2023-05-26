ef537721e2b946682f98e4a7c91aee67c396e832
Chuck LU <chucklu2015@outlook.com>
2023-05-26 23:08:49 +0800

    query data through models

--- 2023/homework1/ttsx/goods/views.py
+++ 2023/homework1/ttsx/goods/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from goods.models import GoodsCategory
 # Create your views here.
 
 def index(request):
-
-    return render(request, 'index.html')
+    categories =  GoodsCategory.objects.all()
+    for category in categories:
+        category.goods_list=category.goodsinfo_set.all().order_by('-id')[:4]
+    print(categories)
+    return render(request, 'index.html',{
+        'categories':categories,
+    })
