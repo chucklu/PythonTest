21fce90b9374dd6782af3a671d4e2904cab221e4
Chuck LU <chucklu2015@outlook.com>
2023-05-26 18:07:45 +0800

    fix csrf error

--- 2023/chapter5/chapter5/views.py
+++ 2023/chapter5/chapter5/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from datetime import date
 from django.http import Http404
 from django.views import View
+from django.middleware.csrf import get_token
 
 def showData(request, urlData):
     d=date.today()
@@ -17,12 +18,14 @@ def showGetData(request):
 
 class useClassView(View):
     news='使用基于类的视图'
-    form_html = '<form name="input" action="" method="post">' \
-                    '请输入数据：<input type="text" name="data">' \
-                    '<input type="submit" value="提交">' \
-                    '</form>'
     def get(self, request):
-        s = f"{self.news}<br>请求的方法为{request.method}<br>{self.form_html}"
+        csrf_token = get_token(request)
+        form_html = f'<form name="input" action="" method="post">' \
+                    f'请输入数据：<input type="text" name="data">' \
+                    f'<input type="hidden" name="csrfmiddlewaretoken" value="{csrf_token}">' \
+                    f'<input type="submit" value="提交">' \
+                    f'</form>'
+        s = f"{self.news}<br>请求的方法为{request.method}<br>{form_html}"
         return HttpResponse(s)
     def post(self, request):
         s = f"{self.news}<br>请求的方法为{request.method}"
