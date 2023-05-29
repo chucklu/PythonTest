489b088ccd445aa9d43458b6ee72da3e16d9c976
Chuck LU <chucklu2015@outlook.com>
2023-05-29 20:03:15 +0800

    show cart info

--- 2023/homework1/ttsx/goods/views.py
+++ 2023/homework1/ttsx/goods/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from goods.models import GoodsCategory
+from goods.models import GoodsInfo
 # Create your views here.
 
 def index(request):
@@ -8,6 +9,51 @@ def index(request):
     for category in categories:
         category.goods_list=category.goodsinfo_set.all().order_by('-id')[:4]
     print(categories)
+
+    # 读取购物车商品列表
+    cart_goods_list = []
+    # 商品总数
+    cart_goods_count = 0
+    for goods_id, goods_num in request.COOKIES.items():
+        if goods_id == 'csrftoken':
+            continue
+        cart_goods = GoodsInfo.objects.get(id=goods_id)
+        cart_goods.goods_num = goods_num
+        cart_goods_list.append(cart_goods)
+        # 累加购物车商品总数
+        cart_goods_count = cart_goods_count + int(goods_num)
+
     return render(request, 'index.html',{
         'categories':categories,
+        'cart_goods_list': cart_goods_list, 
+        'cart_goods_count': cart_goods_count
     })
+
+def detail(request):
+    """商品详细页面"""
+
+    # 获得产品ID
+    goods_id = request.GET.get('id', 1)
+    # 查询该商品
+    goods_data = GoodsInfo.objects.get(id=goods_id)
+    # 查询商品分类
+    categories = GoodsCategory.objects.all()
+
+    # 读取购物车商品列表
+    cart_goods_list = []
+    # 商品总数
+    cart_goods_count = 0
+    for goods_id, goods_num in request.COOKIES.items():
+        if goods_id == 'csrftoken':
+            continue
+
+        cart_goods = GoodsInfo.objects.get(id=goods_id)
+        cart_goods.goods_num = goods_num
+        cart_goods_list.append(cart_goods)
+        # 累加购物车商品总数
+        cart_goods_count += int(goods_num)
+
+    return render(request, 'detail.html', {'categories': categories,
+                                           'goods_data': goods_data,
+                                           'cart_goods_list': cart_goods_list,
+                                           'cart_goods_count': cart_goods_count})
