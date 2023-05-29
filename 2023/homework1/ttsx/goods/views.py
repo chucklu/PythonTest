4860fcab9ff1f74008682e2a64dbf426475cfb22
Chuck LU <chucklu2015@outlook.com>
2023-05-29 20:16:13 +0800

    refactor code

--- 2023/homework1/ttsx/goods/views.py
+++ 2023/homework1/ttsx/goods/views.py
@@ -4,11 +4,17 @@ from goods.models import GoodsCategory
 from goods.models import GoodsInfo
 # Create your views here.
 
-def index(request):
-    categories =  GoodsCategory.objects.all()
+
+def getCategoriesWithLastFourGoods():
+    categories = GoodsCategory.objects.all()
     for category in categories:
-        category.goods_list=category.goodsinfo_set.all().order_by('-id')[:4]
+        category.goods_list = category.goodsinfo_set.all().order_by('-id')[:4]
     print(categories)
+    return categories
+
+
+def getCartData():
+    cart_dict = {}
 
     # 读取购物车商品列表
     cart_goods_list = []
@@ -23,11 +29,22 @@ def index(request):
         # 累加购物车商品总数
         cart_goods_count = cart_goods_count + int(goods_num)
 
-    return render(request, 'index.html',{
-        'categories':categories,
-        'cart_goods_list': cart_goods_list, 
-        'cart_goods_count': cart_goods_count
+    cart_dict['cart_goods_list'] = cart_goods_list
+    cart_dict['cart_goods_count'] = cart_goods_count
+    return cart_dict
+
+
+def index(request):
+
+    categoriesWithLastFourGoods = getCategoriesWithLastFourGoods()
+    cart_dict = getCartData()
+
+    return render(request, 'index.html', {
+        'categories': categoriesWithLastFourGoods,
+        'cart_goods_list': cart_dict['cart_goods_list'],
+        'cart_goods_count': cart_dict['cart_goods_count'],
     })
+
 
 def detail(request):
     """商品详细页面"""
@@ -40,20 +57,9 @@ def detail(request):
     categories = GoodsCategory.objects.all()
 
     # 读取购物车商品列表
-    cart_goods_list = []
-    # 商品总数
-    cart_goods_count = 0
-    for goods_id, goods_num in request.COOKIES.items():
-        if goods_id == 'csrftoken':
-            continue
-
-        cart_goods = GoodsInfo.objects.get(id=goods_id)
-        cart_goods.goods_num = goods_num
-        cart_goods_list.append(cart_goods)
-        # 累加购物车商品总数
-        cart_goods_count += int(goods_num)
+    cart_dict = getCartData()
 
     return render(request, 'detail.html', {'categories': categories,
                                            'goods_data': goods_data,
-                                           'cart_goods_list': cart_goods_list,
-                                           'cart_goods_count': cart_goods_count})
+                                           'cart_goods_list': cart_dict['cart_goods_list'],
+                                           'cart_goods_count': cart_dict['cart_goods_count'],})
