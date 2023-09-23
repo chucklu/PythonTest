a9649d991ae9a0f7d048e29524bead09e794a183
Chuck LU <chucklu2015@outlook.com>
2023-09-23 08:45:53 +0800

    add source code for binary search and selection sort

---
+++ 2023/20230916/SelectionSort.py
@@ -0,0 +1,22 @@
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if (arr[i] < smallest):
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest_index = findSmallest(arr)
+        smallest = arr.pop(smallest_index)
+        newArr.append(smallest)
+    return newArr
+
+
+my_list = [5, 3, 6, 2, 10]
+newArr = selectionSort(my_list)
+print(newArr)
