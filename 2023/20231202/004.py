0da865b771a251d70b7f3f8c48b443d28bd99ccd
Chuck LU <chucklu2015@outlook.com>
2023-12-02 20:57:25 +0800

    level_order, pre_order, in_order, post_order

--- 2023/20231202/004.py
+++ 2023/20231202/004.py
@@ -1,3 +1,4 @@
+from collections import deque
 class TreeNode:
     """二叉树节点类"""
     def __init__(self, val: int):
@@ -5,26 +6,48 @@ class TreeNode:
         self.left: TreeNode | None = None  # 左子节点引用
         self.right: TreeNode | None = None # 右子节点引用
 
-def preorder_traversal(root):
-    """二叉树先序遍历函数 NLR"""
-    result = []
-    stack = []
-    node = root
+def level_order(root: TreeNode | None) -> list[int]:
+    """层序遍历"""
+    # 初始化队列，加入根节点
+    queue: deque[TreeNode] = deque()
+    queue.append(root)
+    # 初始化一个列表，用于保存遍历序列
+    res = []
+    while queue:
+        node: TreeNode = queue.popleft()  # 队列出队
+        res.append(node.val)  # 保存节点值
+        if node.left is not None:
+            queue.append(node.left)  # 左子节点入队
+        if node.right is not None:
+            queue.append(node.right)  # 右子节点入队
+    return res
 
-    while node or stack:
-        while node:
-            # Visit the current node
-            result.append(node.val)
-            # Push right child to stack
-            if node.right:
-                stack.append(node.right)
-            # Move to the left child
-            node = node.left
+def pre_order(root: TreeNode | None, res: list):
+    """前序遍历"""
+    if root is None:
+        return
+    # 访问优先级：根节点 -> 左子树 -> 右子树
+    res.append(root.val)
+    pre_order(root.left, res)
+    pre_order(root.right, res)
 
-        # If no left child, pop from stack (which is the right child)
-        if stack:
-            node = stack.pop()
-    return result
+def in_order(root: TreeNode | None, res: list):
+    """中序遍历"""
+    if root is None:
+        return
+    # 访问优先级：左子树 -> 根节点 -> 右子树
+    in_order(root.left, res)
+    res.append(root.val)
+    in_order(root.right, res)
+
+def post_order(root: TreeNode | None, res: list):
+    """后序遍历"""
+    if root is None:
+        return
+    # 访问优先级：左子树 -> 右子树 -> 根节点
+    post_order(root.left, res)
+    post_order(root.right, res)
+    res.append(root.val)
 
 # 初始化二叉树
 # 初始化节点
@@ -41,5 +64,17 @@ n2.right = n4
 n3.left = n5
 n3.right = n6
 
-preorderResult = preorder_traversal(n1)
+levelorderResult = level_order(n1)
+print(levelorderResult)
+
+preorderResult = []
+pre_order(n1, preorderResult)
 print(preorderResult)
+
+inorderResult = []
+in_order(n1, inorderResult)
+print(inorderResult)
+
+postorderResult = []
+post_order(n1, postorderResult)
+print(postorderResult)
